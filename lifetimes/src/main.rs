29155f4a766af09d2161b4760235c7d6c78810d9
ErikAnkerKilbergSkallevold <erik.skallevold@gmail.com>
2022-07-19 23:40:46 +0200

    Experimented with lifetimes
    It is pain

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,38 @@
+use std::fmt::Display;
+
+fn main() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        let result1 = longest_with_an_announcement(string1.as_str(), string2.as_str(), "Hello There!");
+        println!("The longest string is {}", result);
+        println!("The longest string with Ann is {}", result1);
+    }
+    
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
